9d35100e3897485f96186c2a9608d38cb97c95eb
Dhruv <dhruvmittal52@gmail.com>
2022-08-04 22:15:22 +0530

    commit

--- loot_houses.java
+++ loot_houses.java
@@ -1,4 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class loot_houses {
+    public static int maxMoneyLooted(int[] houses){
+        return maxMoneyLootedHelp(houses,0);
+    }
+    public static int maxMoneyLootedHelp(int[] houses,int i){
+        if(i>houses.length-1){
+            return 0;
+        }
+        int opt1=houses[i]+maxMoneyLootedHelp(houses,i+2);
+        int opt2=maxMoneyLootedHelp(houses,i+1);
+        return Math.max(opt1,opt2);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] input=new int[n];
+        for(int i=0;i<n;i++){
+            input[i]= sc.nextInt();
+        }
+        int ans=maxMoneyLooted(input);
+        System.out.println(ans);
+    }
 }
